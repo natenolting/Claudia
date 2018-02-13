ba4b0057a04f202acf2b328159ec558552faaabe
Claudbell29 <35505213+Claudbell29@users.noreply.github.com>
2018-02-12 19:34:55 -0600

    Febuary_6th

---
+++ Febuary_6th/Endpoint.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>
+    Claudia Contact form
+  </title>
+
+</head>
+  <Body>
+
+Thank you for taking the time to fill out the form. We greatly appreciate it! <
+<?php echo " {$_POST['name']"; ?>
+  </Body>
+</html>
---
+++ Febuary_6th/Index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+  <title>
+    Claudia Contact form
+  </title>
+
+</head>
+  <Body>
+
+<form>
+  <Label>
+    Name
+  </label>
+  <input type="text"><br>
+  <Label>
+    Phone
+  </label>
+  <input type= "tel"><br>
+  <Label>
+    Email
+  </label>
+  <input type= "email"><br>
+  <label>
+    Messages
+  </label>
+    <textarea rows="8" cols="30">
+    </textarea>
+
+</form>
+
+  </Body>
+</html>
